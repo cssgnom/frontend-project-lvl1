a66c2b004bbd419a0a811f84f1f6ecf765b85c99
cssgnom <cssgnom@gmail.com>
2022-02-17 01:06:36 +0300

    add cli.js file

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readline from "readline-sync";
+
+const defaultHello = () => {
+  console.log("Welcome to the Brain Games!");
+  const name = readline.question("May I have your name?: ");
+  console.log(`Hello, ${name}!`);
+};
+
+export default defaultHello;
